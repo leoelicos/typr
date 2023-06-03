4aae9a0d4d877dce0a5836a5ca7e2340d36f859a
Leo Wong <leoelicos@gmail.com>
2023-06-04 01:48:34 +1000

    Remove log

--- src/components/Row/index.jsx
+++ src/components/Row/index.jsx
@@ -1,6 +1,5 @@
 import './style/row.css'
 export default function Row({ children, location }) {
-  console.log({ location })
   const rowType = location === 0 ? 'typing' : ''
   return <div className={`row ${rowType}`}>{children}</div>
 }
