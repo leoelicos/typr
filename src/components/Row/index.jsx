eec13e97506179cdf4a244b05d3a4a0438ed2bf9
Leo Wong <leoelicos@gmail.com>
2023-06-04 23:36:23 +1000

    Update style path

--- src/components/Row/index.jsx
+++ src/components/Row/index.jsx
@@ -1,4 +1,4 @@
-import './style/row.css'
+import './row.scss'
 export default function Row({ children, location }) {
   const rowType = location === 0 ? 'typing' : ''
   return <div className={`row ${rowType}`}>{children}</div>
