3089d050b2ba0aac06235fa5ece65149dd035762
Leo Wong <leoelicos@gmail.com>
2023-06-04 23:57:04 +1000

    Remove log

--- src/components/Key/index.jsx
+++ src/components/Key/index.jsx
@@ -3,6 +3,6 @@ export default function Key({ value, composite }) {
   if (composite)
     // split the value into spans
     value = value.split('').map((character) => <span key={character}>{character}</span>)
-  if (value === 'SPACE') console.log('render space')
+  // if (value === 'SPACE') console.log('render space')
   return <div className={`key ${composite ? 'composite' : ''}`}>{value}</div>
 }
