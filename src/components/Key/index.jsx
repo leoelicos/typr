3156de8f859578f6bed0ec25b3e81a8507319aa4
Leo Wong <leoelicos@gmail.com>
2023-06-04 23:36:57 +1000

    Conditional style for composite keys like {[

--- src/components/Key/index.jsx
+++ src/components/Key/index.jsx
@@ -1,3 +1,8 @@
-export default function Key({ value }) {
-  return <div className='key'>{value}</div>
+import './key.scss'
+export default function Key({ value, composite }) {
+  if (composite)
+    // split the value into spans
+    value = value.split('').map((character) => <span key={character}>{character}</span>)
+  if (value === 'SPACE') console.log('render space')
+  return <div className={`key ${composite ? 'composite' : ''}`}>{value}</div>
 }
