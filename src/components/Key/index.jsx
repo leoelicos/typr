068754de84100fdc8b288bf4498c03681e950780
Leo Wong <leoelicos@gmail.com>
2023-06-05 00:53:17 +1000

    Underscore beneath F and J

--- src/components/Key/index.jsx
+++ src/components/Key/index.jsx
@@ -1,8 +1,15 @@
 import './key.scss'
-export default function Key({ value, composite }) {
+export default function Key({ value, composite, underscore }) {
   if (composite)
     // split the value into spans
-    value = value.split('').map((character) => <span key={character}>{character}</span>)
+    value = value.split('').map((character) => (
+      <span
+        key={character}
+        className={`${character === '@' ? 'atsign' : ''}`}>
+        {character}
+      </span>
+    ))
   // if (value === 'SPACE') console.log('render space')
-  return <div className={`key ${composite ? 'composite' : ''}`}>{value}</div>
+
+  return <div className={`key ${composite ? 'composite' : ''} ${underscore ? 'underscore' : ''}`}>{value}</div>
 }
