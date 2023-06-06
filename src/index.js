0625ecaf5db320df81259c4adcb326ce69beac14
Leo Wong <leoelicos@gmail.com>
2023-06-06 13:54:38 +1000

    Add PressedProvider to the App

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
 
+import PressedProvider from './pressedContext/pressedContext'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <PressedProvider>
+      <App />
+    </PressedProvider>
   </React.StrictMode>
 )
