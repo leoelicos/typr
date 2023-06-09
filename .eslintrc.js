4e3844d6881ff830ced542dd0c79ef27fb726c7b
Leo Wong <leoelicos@gmail.com>
2023-06-09 21:10:13 +1000

    react-in-jsx-scope: off

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     'prettier/prettier': ['error'],
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off'
   },
   settings: {
     react: {
