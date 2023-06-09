5bb569584ab32896ab21776b4a7a57478a60f7cf
Leo Wong <leoelicos@gmail.com>
2023-06-09 21:08:27 +1000

    Change to json

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-// prettier.config.js or .prettierrc.js
-module.exports = {
-  endOfLine: 'crlf',
-  semi: false,
-  trailingComma: 'none',
-  singleQuote: true,
-  printWidth: 120,
-  tabWidth: 2
-}
