5530c5a0a5453eb7d1a9925546ed90d156268c2f
Leo Wong <leoelicos@gmail.com>
2023-06-09 21:11:00 +1000

    Remove unused shiftDisplay

--- src/components/Key/Key.jsx
+++ src/components/Key/Key.jsx
@@ -1,12 +1,8 @@
 import './key.scss'
-export default function Key({ display, shiftDisplay, underscore }) {
-  // if (value === 'SPACE') console.log('display space')
-
+export default function Key({ display, underscore }) {
   return (
     <div className={`key  ${underscore ? 'underscore' : ''}`}>
-      <span
-        key={display}
-        className={`${display === '@' ? 'atsign' : ''}`}>
+      <span key={display} className={`${display === '@' ? 'atsign' : ''}`}>
         {display}
       </span>
     </div>
