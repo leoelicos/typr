574d7c331a5e406834b0cfad43296c7183a9cdfd
Leo Wong <leoelicos@gmail.com>
2023-06-09 20:57:35 +1000

    Rename component names. Adjust spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import './App.scss'
-import Frame from './components/Frame'
-import Keycap from './components/Keycap'
-import Keyboard from './components/Keyboard'
-import Row from './components/Row'
-import Legend from './components/Legend'
-
-import typing from './data/typing'
-
-import useKey from './hooks/useKey.js'
-import { PressedDispatchContext } from './pressedContext/pressedContext'
 import { useContext } from 'react'
+import useKey from './hooks/useKey.js'
+import Frame from './components/Frame/Frame.jsx'
+import Keycap from './components/Keycap/Keycap.jsx'
+import Keyboard from './components/Keyboard/Keyboard.jsx'
+import Row from './components/Row/Row.jsx'
+import Legend from './components/Legend/Legend.jsx'
+import typing from './data/typing.js'
+import { PressedDispatchContext } from './pressedContext/pressedContext'
+import './App.scss'
+
 export default function App() {
   const dispatch = useContext(PressedDispatchContext)
   useKey(dispatch)
@@ -17,18 +16,10 @@ export default function App() {
     <Keyboard>
       <Frame>
         {typing.map((row) => (
-          <Row
-            key={row.rowId}
-            location={row.location}>
+          <Row key={row.rowId} location={row.location}>
             {row.keys.map((key) => (
-              <Keycap
-                key={key.code}
-                type={key.type}>
-                <Legend
-                  code={key.code}
-                  display={key.display}
-                  shiftDisplay={key.shiftDisplay}
-                />
+              <Keycap key={key.code} type={key.type}>
+                <Legend code={key.code} display={key.display} shiftDisplay={key.shiftDisplay} />
               </Keycap>
             ))}
           </Row>
@@ -37,3 +28,4 @@ export default function App() {
     </Keyboard>
   )
 }
+
