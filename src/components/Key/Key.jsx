da2dd8d44c29d3d550fd3318c1f20e59cf7c4f64
Leo Wong <leoelicos@gmail.com>
2023-06-11 01:56:46 +1000

    Remove useless span

--- src/components/Key/Key.jsx
+++ src/components/Key/Key.jsx
@@ -1,11 +1,5 @@
 import './key.scss'
 
 export default function Key({ display, underscore }) {
-  return (
-    <div className={`key  ${underscore ? 'underscore' : ''}`}>
-      <span key={display} className="display">
-        {display}
-      </span>
-    </div>
-  )
+  return <div className={`key  ${underscore ? 'underscore' : ''}`}>{display}</div>
 }
