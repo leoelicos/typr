ef4bd33029ab8d0a001dd9950209e6d57f9eb40a
Leo Wong <leoelicos@gmail.com>
2023-06-10 20:10:07 +1000

    Lift context consumer up to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useMemo } from 'react'
 import useKey from './hooks/useKey.js'
 import Frame from './components/Frame/Frame.jsx'
 import Keycap from './components/Keycap/Keycap.jsx'
@@ -6,24 +6,34 @@ import Keyboard from './components/Keyboard/Keyboard.jsx'
 import Row from './components/Row/Row.jsx'
 import Legend from './components/Legend/Legend.jsx'
 import typing from './data/typing.js'
-import { PressedDispatchContext } from './pressedContext/pressedContext'
+import { PressedContext, PressedDispatchContext } from './pressedContext/pressedContext'
 import './App.scss'
+import Key from './components/Key/Key.jsx'
 
 export default function App() {
   const dispatch = useContext(PressedDispatchContext)
   useKey(dispatch)
+  const state = useContext(PressedContext)
+  const shifted = state.ShiftLeft || state.ShiftRight
+  const blanks = useMemo(() => ['Tab', 'ControlLeft', 'MetaLeft', 'AltLeft', 'AltRight', 'MetaRight', 'ContextMenu', 'ControlRight'], [])
   return (
     <Keyboard>
       <Frame>
-        {typing.map((row) => (
-          <Row key={row.rowId} location={row.location}>
-            {row.keys.map((key) => (
-              <Keycap key={key.code} type={key.type}>
-                <Legend code={key.code} display={key.display} shiftDisplay={key.shiftDisplay} />
-              </Keycap>
-            ))}
-          </Row>
-        ))}
+        {typing.map((row) => {
+          return (
+            <Row key={row.rowId} location={row.location}>
+              {row.keys.map((key) => {
+                return (
+                  <Keycap key={key.code} type={key.type}>
+                    <Legend isBlank={blanks.includes(key.code)} isPressed={state[key.code]}>
+                      <Key display={shifted ? key.shiftDisplay : key.display} underscore={['KeyF', 'KeyJ'].includes(key.code)} />
+                    </Legend>
+                  </Keycap>
+                )
+              })}
+            </Row>
+          )
+        })}
       </Frame>
     </Keyboard>
   )
