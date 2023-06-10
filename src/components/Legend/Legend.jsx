e3c76ab21793b327dc958f89514ac5052a258740
Leo Wong <leoelicos@gmail.com>
2023-06-10 20:11:32 +1000

    Lift context consumer up to App

--- src/components/Legend/Legend.jsx
+++ src/components/Legend/Legend.jsx
@@ -1,19 +1,5 @@
-import Key from '../Key/Key'
 import './legend.scss'
-import { useContext, useMemo } from 'react'
-import { PressedContext } from '../../pressedContext/pressedContext'
 
-const Legend = ({ code, display, shiftDisplay }) => {
-  const state = useContext(PressedContext)
-
-  const shifted = state['ShiftLeft'] || state['ShiftRight']
-  const isPressed = state[code]
-  const pressedClass = useMemo(() => (isPressed ? 'pressed' : ''), [isPressed])
-  return (
-    <div className={`legend ${display ? '' : 'blank'} ${pressedClass}`}>
-      <Key display={shifted ? shiftDisplay : display} underscore={display === 'F' || display === 'J'} />
-    </div>
-  )
+export default function Legend({ isBlank, isPressed, children }) {
+  return <div className={`legend ${isBlank ? 'blank' : ''} ${isPressed ? 'pressed' : ''}`}>{children}</div>
 }
-
-export default Legend
