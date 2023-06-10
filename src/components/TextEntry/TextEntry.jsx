cb2db4b041e6da25f32062bf3602c3fd28dd1832
Leo Wong <leoelicos@gmail.com>
2023-06-11 01:58:48 +1000

    Define textarea element

---
+++ src/components/TextEntry/TextEntry.jsx
@@ -0,0 +1,8 @@
+import './textentry.scss'
+export default function TextEntry() {
+  return (
+    <div className="text-entry-wrapper">
+      <textarea type="text" className="text-entry" />
+    </div>
+  )
+}
