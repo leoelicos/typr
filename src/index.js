5c885d8c3f197ccf8b6355e0186702fe9f03b033
Leo Wong <leoelicos@gmail.com>
2023-06-12 20:51:34 +1000

    Move Provider down

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
 
-import PressedProvider from './pressedContext/pressedContext'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <PressedProvider>
-      <App />
-    </PressedProvider>
+    <App />
   </React.StrictMode>
 )
